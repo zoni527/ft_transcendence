53e849e384f7219af64b64e9a42ff2998bb599e6
Johnny Varila <jvarila@student.hive.fi>
2026-04-02 11:55:48 +0300

    feat(backend): initial user and recipe endpoints with hardcoded data, initial Makefile and .gitignore
    First version of non containerized Go backend that has simple GET, POST
    and PATCH endpoints for /api/users[/:id] and /api/recipes[/:id]. Data is
    hardcoded, all fields are strings at the moment, and the recipe doesn't
    have all fields yet. Steps for the future: containerization, more
    fields, calls to database instead of hardcoded data, figure out testing
    (nc, curl, python...).

---
+++ Makefile
@@ -0,0 +1,15 @@
+SRC_DIR			:= ./src
+BACKEND_EXEC	:= backend
+
+all: run_backend
+
+run_backend: ${SRC_DIR}/backend/${BACKEND_EXEC}
+	${SRC_DIR}/backend/${BACKEND_EXEC}
+
+${SRC_DIR}/backend/${BACKEND_EXEC}:
+	cd ${SRC_DIR}/backend; go build -o ${BACKEND_EXEC}
+
+fclean:
+	rm -f ${SRC_DIR}/backend/${BACKEND_EXEC}
+
+.PHONY: all clean fclean re run_backend
---
+++ src/backend/main.go
@@ -0,0 +1,234 @@
+// -------------------------------------------------------------------------- //
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+// -------------------------------------------------------------------------- //
+// Structs
+
+type user struct {
+	Id            string `json:"id"`
+	Email         string `json:"email"`
+	Password_hash string `json:"password_hash"`
+	Name          string `json:"name"`
+	Display_name  string `json:"display_name"`
+	Created_at    string `json:"created_at"`
+}
+
+type recipe struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Created_at  string `json:"created_at"`
+	Updated_at  string `json:"updated_at"`
+}
+
+// -------------------------------------------------------------------------- //
+// Utility functions
+
+// Not used currently, hardcoded IDs for now, would have to be called through main
+// (in reality this would be the backends job).
+func getIdGenerator() func() uint64 {
+	id := uint64(0)
+	f := func() uint64 {
+		ret := id
+		id++
+		return ret
+	}
+	return f
+}
+
+// -------------------------------------------------------------------------- //
+// Data placeholders, will be fetched from the backend later.
+
+var users = []user{
+	{Id: "0", Email: "Margin0@gmail.com", Password_hash: "000", Name: "Marvin0", Display_name: "marV0", Created_at: "now"},
+	{Id: "1", Email: "Margin1@gmail.com", Password_hash: "001", Name: "Marvin1", Display_name: "marV1", Created_at: "now"},
+	{Id: "2", Email: "Margin2@gmail.com", Password_hash: "002", Name: "Marvin2", Display_name: "marV2", Created_at: "now"},
+	{Id: "3", Email: "Margin3@gmail.com", Password_hash: "003", Name: "Marvin3", Display_name: "marV3", Created_at: "now"},
+	{Id: "4", Email: "Margin4@gmail.com", Password_hash: "004", Name: "Marvin4", Display_name: "marV4", Created_at: "now"},
+	{Id: "5", Email: "Margin5@gmail.com", Password_hash: "005", Name: "Marvin5", Display_name: "marV5", Created_at: "now"},
+	{Id: "6", Email: "Margin6@gmail.com", Password_hash: "006", Name: "Marvin6", Display_name: "marV6", Created_at: "now"},
+	{Id: "7", Email: "Margin7@gmail.com", Password_hash: "007", Name: "Marvin7", Display_name: "marV7", Created_at: "now"},
+	{Id: "8", Email: "Margin8@gmail.com", Password_hash: "008", Name: "Marvin8", Display_name: "marV8", Created_at: "now"},
+	{Id: "9", Email: "Margin9@gmail.com", Password_hash: "009", Name: "Marvin9", Display_name: "marV9", Created_at: "now"},
+}
+
+var recipes = []recipe{
+	{Id: "0", Name: "Recipe0", Description: "Description0"},
+	{Id: "1", Name: "Recipe1", Description: "Description1"},
+	{Id: "2", Name: "Recipe2", Description: "Description2"},
+	{Id: "3", Name: "Recipe3", Description: "Description3"},
+	{Id: "4", Name: "Recipe4", Description: "Description4"},
+	{Id: "5", Name: "Recipe5", Description: "Description5"},
+	{Id: "6", Name: "Recipe6", Description: "Description6"},
+	{Id: "7", Name: "Recipe7", Description: "Description7"},
+	{Id: "8", Name: "Recipe8", Description: "Description8"},
+}
+
+// -------------------------------------------------------------------------- //
+
+func main() {
+	fmt.Println("ft_transcendence")
+
+	port := 8080
+	err := error(nil)
+	argc := len(os.Args)
+	switch {
+	case argc >= 2:
+		port, err = strconv.Atoi(os.Args[1])
+	default:
+	}
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	router := gin.Default()
+
+	router.GET("/api/users", getUsers)
+	router.GET("/api/users/:id", getUserById)
+	router.GET("/api/recipes", getRecipes)
+	router.GET("/api/recipes/:id", getRecipeById)
+
+	router.POST("/api/users", postUsers)
+	router.POST("/api/recipes", postRecipes)
+
+	router.PATCH("/api/users", patchUsers)
+
+	router.Run("localhost:" + strconv.Itoa(port))
+}
+
+// -------------------------------------------------------------------------- //
+// GET
+
+// ---- All
+
+func getUsers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users)
+}
+
+func getRecipes(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, recipes)
+}
+
+// ---- By Id
+
+func getUserById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, u := range users {
+		if u.Id == id {
+			c.IndentedJSON(http.StatusOK, u)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
+}
+
+func getRecipeById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, u := range recipes {
+		if u.Id == id {
+			c.IndentedJSON(http.StatusOK, u)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "recipe not found"})
+}
+
+// -------------------------------------------------------------------------- //
+// POST
+
+func postUsers(c *gin.Context) {
+	var newUser user
+
+	// Construct user object from received JSON
+	if err := c.BindJSON(&newUser); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Field validation
+	if newUser.Email == "" ||
+		newUser.Name == "" ||
+		newUser.Display_name == "" {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{
+			"message": "bad request",
+			"reason":  "empty required field",
+		})
+		return
+	}
+
+	newUser.Id = strconv.Itoa(len(users))       // Id forming
+	users = append(users, newUser)              // Adding new user to DB
+	c.IndentedJSON(http.StatusCreated, newUser) // Response
+}
+
+func postRecipes(c *gin.Context) {
+	var newRecipe recipe
+
+	// Construct recipe object from received JSON
+	if err := c.BindJSON(&newRecipe); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Field validation
+	if newRecipe.Name == "" ||
+		newRecipe.Description == "" {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{
+			"message": "bad request",
+			"reason":  "empty required field",
+		})
+		return
+	}
+
+	newRecipe.Id = strconv.Itoa(len(recipes))     // Id forming
+	recipes = append(recipes, newRecipe)          // Adding new recipe to DB
+	c.IndentedJSON(http.StatusCreated, newRecipe) // Response
+}
+
+// -------------------------------------------------------------------------- //
+// PATCH
+
+func patchUsers(c *gin.Context) {
+	id := c.Param("id")
+	var mod *user
+
+	for _, u := range users {
+		if u.Id == id {
+			mod = &u
+		}
+	}
+
+	if mod == nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
+		return
+	}
+
+	params := c.Request.URL.Query()
+	for k, v := range params {
+		switch k {
+		case "email":
+			mod.Email = v[0]
+		case "name":
+			mod.Name = v[0]
+		case "display_name":
+			mod.Display_name = v[0]
+		}
+	}
+
+	c.IndentedJSON(http.StatusOK, mod)
+}
+
+// -------------------------------------------------------------------------- //

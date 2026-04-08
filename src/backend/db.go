1495b0f434419a9b455fcbe35a0c46ef04ef4533
Hien Nguyen <hien.nguyen2@myy.haaga-helia.fi>
2026-04-08 16:50:59 +0300

    feat: add GetAllUsers() query function in db.go

--- src/backend/db.go
+++ src/backend/db.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+//THIS IS THE DATABASE LAYER CODE. main.go us
 // DB is the connection pool shared across the application.
 // A pool manages multiple connections so the backend can handle
 // concurrent requests without opening a new connection each time.
@@ -44,4 +45,42 @@ func CloseDB() {
 	if DB != nil {
 		DB.Close()
 	}
+}
+
+//http://betterstack.com/community/guides/scaling-go/postgresql-pgx-golang/
+func GetAllUsers() ([]user, error) {
+	sql := `SELECT id, email, name, display_name, created_at
+			FROM "user" `
+
+	//DB.Query() returns a pgx.Rows object point to the result set of db.
+	rows, err := DB.Query(context.Background(), sql)
+	if err != nil {
+		return nil, fmt.Errorf("error querying users: %w", err)
+    }
+
+	// releases the database connection back to the pool. If you forget to close, you'll leak connections and eventually run out.
+	defer rows.Close()
+	
+	var users []user
+	//step through rows one by one with
+	for rows.Next() {
+		var u user
+		err := rows.Scan(
+			&u.Id,
+			&u.Email,
+			&u.Name,
+			&u.Display_name,
+			&u.Created_at,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("error scanning user row: %w", err)
+		}
+		users = append(users, u)
+	}
+
+	//declaring err, then check if it's nil
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("error iterating user rows: %w", err)
+	}
+	return users, nil
 }

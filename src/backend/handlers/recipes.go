97c36d1c397f6601a0f656f5e3afa6417ebb8009
Johnny Varila <jvarila@student.hive.fi>
2026-04-16 17:41:57 +0300

    fix(backend): improved validation and communication with backend for POST /api/recipes endpoint
    Honed validation for almost all fields: difficulty and meal type
    validation now match the database requirements, string fields have
    maximum lengths, number field validation refactored a bit.

--- src/backend/handlers/recipes.go
+++ src/backend/handlers/recipes.go
@@ -64,53 +64,20 @@ func CreateRecipe(c *gin.Context) {
 	// Check if author_id exists in database, if not, no go
 	if _, err := repository.GetUserById(r.Author_id); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{
-			"error": fmt.Sprintf("author `%v' not in database", r.Author_id),
+			"error": fmt.Sprintf("author_id %v not in database", r.Author_id),
 		})
 		return
 	}
 
-	if err := IsValidTitle(&r.Title); err != nil {
+	if err := ValidateRecipeFields(&r); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("%v", err)})
-		return
-	}
-
-	// --------------
-	// int parameters
-	// --------------
-
-	// I know these values are ridiculous, but with this you could feed 100 sumo wrestlers
-	const PREP_TIME_MAX = 60 * 1000 // 1000 hours max
-	const COOK_TIME_MAX = 60 * 100  // 100 hours max
-	const SERVINGS_MAX = 100
-	const CALORIES_MAX = 1000000
-
-	switch {
-	case !intFieldOk(c, &r.Prep_time_min, "prep time", 0, PREP_TIME_MAX),
-		!intFieldOk(c, &r.Cook_time_min, "cook time", 0, COOK_TIME_MAX),
-		!intFieldOk(c, &r.Servings, "servings", 1, SERVINGS_MAX),
-		!intFieldOk(c, &r.Calories, "calories", 0, CALORIES_MAX): // Allows one serving of ice water
-		return
-	}
-
-	// ------------------
-	// float64 parameters
-	// ------------------
-
-	const PROTEIN_MAX = 1000 * 100 // 100 kg
-	const CARBS_MAX = 1000 * 100   // 100 kg
-	const FAT_MAX = 1000 * 100     // 100 kg
-
-	switch {
-	case !floatFieldOk(c, &r.Protein_g, "protein grams", 0, PROTEIN_MAX),
-		!floatFieldOk(c, &r.Carbs_g, "carbs grams", 0, CARBS_MAX),
-		!floatFieldOk(c, &r.Fat_g, "calories", 0, FAT_MAX):
 		return
 	}
 
 	newRecipe, err := repository.CreateRecipe(&r)
 	if err != nil {
 		c.IndentedJSON(http.StatusInternalServerError, gin.H{
-			"error": fmt.Sprintf("internal server error: %w", err),
+			"error": fmt.Sprintf("internal server error: %v", err),
 		})
 	}
 	c.IndentedJSON(http.StatusOK, newRecipe)
@@ -120,42 +87,164 @@ func CreateRecipe(c *gin.Context) {
 // helper functions
 // ----------------
 
-func intFieldOk(c *gin.Context, field *int, fieldName string, fieldMin, fieldMax int) bool {
-	if *field < fieldMin || *field > fieldMax {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{
-			"error": fmt.Sprintf("bad %v value: %v", fieldName, *field),
-		})
-		return false
-	}
-	return true
-}
+// I know these values are ridiculous, but with this you could feed 100 sumo wrestlers
+const PREP_TIME_MAX = 60 * 1000 // 1000 hours max
+const COOK_TIME_MAX = 60 * 100  // 100 hours max
+const SERVINGS_MAX = 100
+const CALORIES_MAX = 1000000
 
-func floatFieldOk(c *gin.Context, field *float64, fieldName string, fieldMin, fieldMax float64) bool {
-	if *field < fieldMin || *field > fieldMax {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{
-			"error": fmt.Sprintf("bad %v value: %v", fieldName, *field),
-		})
-		return false
-	}
-	return true
-}
+const PROTEIN_MAX = 1000 * 100 // 100 kg
+const CARBS_MAX = 1000 * 100   // 100 kg
+const FAT_MAX = 1000 * 100     // 100 kg
 
-const TITLE_MAX_LEN = 20
+const TITLE_LEN_MIN = 3
+const TITLE_LEN_MAX = 60
+const DESCRIPTION_LEN_MAX = 10000
+const CUISINE_LEN_MAX = 50
+const IMAGE_URL_LEN_MAX = 100
 
-func IsValidTitle(t *string) error {
-	if *t == "" {
-		return fmt.Errorf("title: empty string")
+func ValidateRecipeFields(r *models.Recipe) error {
+
+	type intValidation struct {
+		val      int
+		s        string
+		min, max int
 	}
 
-	if len(*t) > TITLE_MAX_LEN {
-		return fmt.Errorf("title too long")
+	// Minimum food: 1 serving of ice water
+	intFields := []intValidation{
+		{r.Prep_time_min, "prep_time_min", 0, PREP_TIME_MAX},
+		{r.Cook_time_min, "cook_time_min", 0, COOK_TIME_MAX},
+		{r.Servings, "servings", 1, SERVINGS_MAX},
+		{r.Calories, "calories", 0, CALORIES_MAX},
 	}
 
-	for _, c := range *t {
-		if !(unicode.IsLetter(c) || unicode.IsNumber(c) || c == ' ') {
-			return fmt.Errorf("forbidden title character: %v", c)
+	for _, v := range intFields {
+		if err := NumFieldOk(v.val, v.s, v.min, v.max); err != nil {
+			return err
 		}
 	}
 
+	// ------------------
+	// float64 parameters
+	// ------------------
+
+	type floatValidation struct {
+		val      float64
+		s        string
+		min, max float64
+	}
+
+	floatFields := []floatValidation{
+		{r.Protein_g, "progein_g", 0, PROTEIN_MAX},
+		{r.Carbs_g, "carbs_g", 0, CARBS_MAX},
+		{r.Fat_g, "fat_g", 0, FAT_MAX},
+	}
+
+	for _, v := range floatFields {
+		if err := NumFieldOk(v.val, v.s, v.min, v.max); err != nil {
+			return err
+		}
+	}
+
+	// -----------------
+	// string parameters
+	// -----------------
+
+	type stringLenValidation struct {
+		field  *string
+		tag    string
+		maxLen int
+	}
+
+	// If some of these values are shared we could put them in a config/env file
+
+	maxStringLengths := []stringLenValidation{
+		{&r.Title, "title", TITLE_LEN_MAX},
+		{&r.Description, "description", DESCRIPTION_LEN_MAX},
+		{&r.Cuisine, "cuisine", CUISINE_LEN_MAX},
+		{&r.Image_url, "image_url", IMAGE_URL_LEN_MAX},
+	}
+
+	for _, v := range maxStringLengths {
+		if len(*v.field) > v.maxLen {
+			return fmt.Errorf("%v: too long", v.tag)
+		}
+	}
+
+	if err := IsValidTitle(&r.Title); err != nil {
+		return fmt.Errorf("title: %w", err)
+	}
+
+	if err := OnlyGraphicChars(&r.Description); err != nil {
+		return fmt.Errorf("description: %w", err)
+	}
+
+	switch r.Difficulty {
+	case "easy", "medium", "hard":
+	default:
+		return fmt.Errorf("difficulty: must be easy, medium, or hard")
+	}
+
+	for _, c := range r.Cuisine {
+		if !(unicode.IsLetter(c) ||
+			unicode.IsSymbol(c) ||
+			unicode.IsPunct(c) ||
+			c == ' ') {
+			return fmt.Errorf("cuisine: bad character: %v", c)
+		}
+	}
+
+	switch r.Meal_type {
+	case "breakfast", "lunch", "dinner", "snack":
+	default:
+		return fmt.Errorf("meal_type: must be breakfast, lunch, dinner, or snack")
+	}
+
+	if err := OnlyGraphicChars(&r.Image_url); err != nil {
+		return fmt.Errorf("image_url: %w", err)
+	}
+
+	return nil
+}
+
+func NumFieldOk[T int | float64](field T, fieldName string, fieldMin, fieldMax T) error {
+	if field < fieldMin || field > fieldMax {
+		return fmt.Errorf("%v: bad  value: %v", fieldName, field)
+	}
+	return nil
+}
+
+func IsValidTitle(t *string) error {
+	if *t == "" {
+		return fmt.Errorf("empty string")
+	}
+
+	switch {
+	case len(*t) > TITLE_LEN_MAX:
+		return fmt.Errorf("too long")
+	case len(*t) < TITLE_LEN_MIN:
+		return fmt.Errorf("too short")
+	}
+
+	for _, c := range *t {
+		if !(unicode.IsLetter(c) ||
+			unicode.IsNumber(c) ||
+			unicode.IsSymbol(c) ||
+			unicode.IsPunct(c) ||
+			c == ' ') {
+			return fmt.Errorf("forbidden character: %v", c)
+		}
+	}
+
+	return nil
+}
+
+func OnlyGraphicChars(s *string) error {
+	for _, c := range *s {
+		if !unicode.IsGraphic(c) {
+			return fmt.Errorf("invalid character: %v", c)
+		}
+	}
 	return nil
 }
--- src/backend/repository/recipes.go
+++ src/backend/repository/recipes.go
@@ -107,33 +107,35 @@ func GetRecipeById(id string) (models.Recipe, error) {
 }
 
 func CreateRecipe(r *models.Recipe) (*models.Recipe, error) {
-	var newRecipe models.Recipe
-
 	sql := `
 		INSERT INTO recipe (
-			author_id, description, prep_time_min, cook_time_min, servings,
-			difficulty, cuisine, meal_type, calories, protein_g,
-			carbs_g, fat_g, is_published
+			author_id, title, description, prep_time_min, cook_time_min,
+			servings, difficulty, cuisine, meal_type, image_url,
+			calories, protein_g, carbs_g, fat_g, is_published
 		) VALUES (
-			$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13
-		) RETURNING id, author_id, description, prep_time_min, cook_time_min, servings,
-			difficulty, cuisine, meal_type, calories, protein_g,
-			carbs_g, fat_g, is_published`
+			$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15
+		) RETURNING id,
+			author_id, title, description, prep_time_min, cook_time_min,
+			servings, difficulty, cuisine, meal_type, image_url,
+			calories, protein_g, carbs_g, fat_g, is_published,
+			created_at, updated_at`
 
-	rows, err := Pool.Query(context.Background(), sql,
-		r.Author_id, r.Description, r.Prep_time_min, r.Cook_time_min, r.Servings,
-		r.Difficulty, r.Cuisine, r.Meal_type, r.Calories, r.Protein_g,
-		r.Carbs_g, r.Fat_g, r.Is_published,
+	var newR models.Recipe
+
+	err := Pool.QueryRow(context.Background(), sql,
+		r.Author_id, r.Title, r.Description, r.Prep_time_min, r.Cook_time_min,
+		r.Servings, r.Difficulty, r.Cuisine, r.Meal_type, r.Image_url,
+		r.Calories, r.Protein_g, r.Carbs_g, r.Fat_g, r.Is_published,
+	).Scan(
+		&newR.Id,
+		&newR.Author_id, &newR.Title, &newR.Description, &newR.Prep_time_min, &newR.Cook_time_min,
+		&newR.Servings, &newR.Difficulty, &newR.Cuisine, &newR.Meal_type, &newR.Image_url,
+		&newR.Calories, &newR.Protein_g, &newR.Carbs_g, &newR.Fat_g, &newR.Is_published,
+		&newR.Created_at, &newR.Updated_at,
 	)
 	if err != nil {
 		return nil, fmt.Errorf("error creating recipe: %w", err)
 	}
-	defer rows.Close()
 
-	newRecipe, err = pgx.RowToStructByName[models.Recipe](rows)
-	if err != nil {
-		return nil, fmt.Errorf("error creating recipe: %w", err)
-	}
-
-	return &newRecipe, nil
+	return &newR, nil
 }

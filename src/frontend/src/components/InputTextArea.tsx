1e1ab5c63399125a625c7d2ffeb373cbd4e74775
Boris Gazur <barlukh@gmail.com>
2026-04-16 13:17:50 +0300

    feature: add InputTextArea component and sync it with InputField

--- src/frontend/src/components/InputTextArea.tsx
+++ src/frontend/src/components/InputTextArea.tsx
@@ -1,35 +1,35 @@
 import { cardBase, inputFieldBase } from '../styles/styles';
 
-interface InputFieldProps {
+interface InputTextAreaProps {
   id: string;
   name: string;
   label: string;
-  type?: string;
+  rows?: number;
   placeholder?: string;
 }
 
-const InputField = ({
+const InputTextArea = ({
   id,
   name,
   label,
-  type = 'text',
+  rows = 4,
   placeholder = '...',
-}: InputFieldProps) => {
+}: InputTextAreaProps) => {
   return (
     <div>
       <label htmlFor={id} className="text-md block font-medium text-gray-700">
         {label}
       </label>
 
-      <input
+      <textarea
         id={id}
         name={name}
-        type={type}
+        rows={rows}
         placeholder={placeholder}
-        className={`${cardBase} ${inputFieldBase}`}
+        className={`${cardBase} ${inputFieldBase} resize-none`}
       />
     </div>
   );
 };
 
-export default InputField;
+export default InputTextArea;

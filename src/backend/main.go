80a1b99915ddf3f3f87ab014a34106b0f27f0b69
Lucio <lucio.surcotaype@outlook.com>
2026-04-28 21:47:33 +0300

    feat: add middleware to check roles, update loading of cloudinary env vars

--- src/backend/main.go
+++ src/backend/main.go
@@ -21,11 +21,15 @@ func main() {
 
 	handlers.LoadJWTSecret()
 
+	err = handlers.LoadCloudinaryVars()
+	if err != nil {
+		log.Fatal("Cloudinary API key:", err)
+	}
 
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:5173"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
 	}))
@@ -35,7 +39,6 @@ func main() {
 	router.GET("/api/users/:id", handlers.GetUserById)
 	router.POST("/api/users", handlers.CreateUser)
 	router.PUT("/api/users/:id", handlers.UpdateUser)     // not implemented yet
-	router.PATCH("/api/users/:id", handlers.PatchUser)    // not implemented yet
 	router.DELETE("/api/users/:id", handlers.DeleteUser)  // not implemented yet
 	router.GET("/api/users/search", handlers.SearchUsers) // not implemented yet
 	router.POST("/api/users/login", handlers.LoginUser)
@@ -45,11 +48,17 @@ func main() {
 	router.GET("/api/recipes", handlers.GetAllRecipes)
 	router.GET("/api/recipes/:id", handlers.GetRecipeById)
 	router.POST("/api/recipes", handlers.CreateRecipe)
-	router.PUT("/api/recipes/:id", handlers.UpdateRecipe)             // not implemented yet
-	router.PATCH("/api/recipes/:id", handlers.PatchRecipe)            // not implemented yet
-	router.DELETE("/api/recipes/:id", handlers.DeleteRecipe)          // not implemented yet
-	router.GET("/api/recipes/imagesignature", handlers.AuthMiddleware(), handlers.GetImageSignature)
-	router.POST("/api/recipes/:id/image", handlers.AuthMiddleware(), handlers.UploadRecipeImage)
+	router.PUT("/api/recipes/:id", handlers.UpdateRecipe)    // not implemented yet
+	router.DELETE("/api/recipes/:id", handlers.DeleteRecipe) // not implemented yet
+	router.GET("/api/recipes/image-signature",
+		handlers.AuthMiddleware(),
+		handlers.RequiredRolesMiddleware("chef", "moderator", "admin"),
+		handlers.RecipeImageSignature)
+
+	router.POST("/api/recipes/:id/image",
+		handlers.AuthMiddleware(),
+		handlers.RequiredRolesMiddleware("chef", "moderator", "admin"),
+		handlers.UploadRecipeImage)
 
 	if err := router.Run("0.0.0.0:8080"); err != nil {
 		log.Fatal("Server failed to start:", err)

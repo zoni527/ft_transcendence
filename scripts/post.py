43fa803ae0e7fdc6ec3652ef3c6ae946988db4bd
Johnny Varila <jvarila@student.hive.fi>
2026-05-07 16:20:35 +0300

    feat: helper python script for sending POST requests

---
+++ scripts/post.py
@@ -0,0 +1,18 @@
+#!/bin/env python3
+
+import requests
+import json
+
+payload = {
+    "user_id": "d809c4dc-0c6e-43c7-bcac-f389a1e1315e",
+    "recipe_id": "b72c5aae-1a84-42e3-a851-9475cca090dc"
+}
+
+url = 'http://localhost:8080/api/favourite'
+
+r = requests.post(url, json = payload)
+print('Status code:', r.status_code)
+print(json.dumps(dict(r.headers), indent=2))
+if len(r.text):
+    parsed = json.loads(r.text)
+    print(json.dumps(parsed, indent=2))

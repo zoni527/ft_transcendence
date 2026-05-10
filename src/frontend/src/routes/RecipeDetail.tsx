c7e62db591bfdd81f03098f315430cdfd0f18140
Boris Gazur <barlukh@gmail.com>
2026-05-10 12:02:51 +0300

    style: RecipeDetail mobile support

--- src/frontend/src/routes/RecipeDetail.tsx
+++ src/frontend/src/routes/RecipeDetail.tsx
@@ -107,27 +107,27 @@ const RecipeDetail = () => {
         {/* Recipe Info Fields */}
         <div className="mt-6 flex flex-col space-y-8 md:flex-row md:space-y-0 md:space-x-8">
           {/* Left Column */}
-          <div className="flex-1 space-y-6 border-b border-gray-300 pb-4 md:border-r md:border-b-0 md:pr-4">
+          <div className="flex-1 space-y-6 border-b border-gray-300 pb-8 md:border-r md:border-b-0 md:pr-4 md:pb-4">
             <DataField
               label={t('recipeDetail.author')}
               value={recipe.author_id}
             />
+
             <DataField
               label={t('recipeDetail.prep')}
               value={recipe.preparation_time_min}
             />
-            <DataField
-              label={t('recipeDetail.servings')}
-              value={recipe.servings}
-            />
+
             <DataField
               label={t('difficulty.type')}
               value={t(`difficulty.type_${recipe.difficulty}`)}
             />
+
             <DataField
               label={t('recipeDetail.cuisine')}
               value={recipe.cuisine}
             />
+
             <DataField
               label={t('meal.type')}
               value={t(`meal.type_${recipe.meal_type}`)}
@@ -137,47 +137,28 @@ const RecipeDetail = () => {
           {/* Right Column */}
           <div className="flex-1 space-y-6 md:pl-4">
             <DataField
+              label={t('recipeDetail.servings')}
+              value={recipe.servings}
+            />
+
+            <DataField
               label={t('recipeDetail.calories')}
               value={recipe.calories}
             />
+
             <DataField
               label={t('recipeDetail.protein')}
               value={recipe.protein_g}
             />
+
             <DataField label={t('recipeDetail.carbs')} value={recipe.carbs_g} />
+
             <DataField label={t('recipeDetail.fat')} value={recipe.fat_g} />
           </div>
         </div>
 
-        {/* Likes Section */}
-        <div className="mt-6 flex flex-col gap-2 sm:flex-row sm:items-center sm:gap-2">
-          <DataField
-            label={t('recipeDetail.likes')}
-            value={'PLACEHOLDER VALUE'}
-          />
-          <button
-            className="text-amber-500 transition-colors hover:cursor-pointer hover:text-amber-600 hover:shadow-xl"
-            aria-label="Like"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              className="h-6 w-6"
-            >
-              <path
-                d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 
-                   2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 
-                   4.5 2.09C13.09 3.81 14.76 3 16.5 3 
-                   19.58 3 22 5.42 22 8.5c0 3.78-3.4 
-                   6.86-8.55 11.54L12 21.35z"
-              />
-            </svg>
-          </button>
-        </div>
-
         {/* Bottom Buttons */}
-        <div className="mt-8 flex flex-col gap-4 md:flex-row md:items-center md:justify-end">
+        <div className="mt-16 flex flex-col gap-4 md:flex-row md:items-center md:justify-end">
           <ModalButton
             className="order-1 rounded-xl border-2 border-slate-600 hover:border-slate-950 md:order-0"
             onClick={() => setIsEditRecipeOpen(true)}

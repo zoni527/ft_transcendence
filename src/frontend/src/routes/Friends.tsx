c2d12dd06ddb5750c4623fa1a36df6f389be5c58
Boris Gazur <barlukh@gmail.com>
2026-05-11 09:06:37 +0300

    fix: uncomment online offline and polish heartbeat api

--- src/frontend/src/routes/Friends.tsx
+++ src/frontend/src/routes/Friends.tsx
@@ -82,13 +82,11 @@ const Friends = () => {
         </div>
 
         {/* Online/Offline Indicator */}
-        {/* className={`absolute top-8 right-8 h-4 w-4 rounded-full border-2 border-slate-950 ${
-              user.is_online ? 'bg-green-500' : 'bg-red-500'
-            }`}
-            title={user.is_online ? 'Online' : 'Offline'} */}
         <div
-          className={`absolute top-8 right-8 h-4 w-4 rounded-full border-2 border-slate-950 bg-green-500`}
-          title={'Online'}
+          className={`absolute top-8 right-8 h-4 w-4 rounded-full border-2 border-slate-950 ${
+            user.is_online ? 'bg-green-500' : 'bg-red-500'
+          }`}
+          title={user.is_online ? 'Online' : 'Offline'}
         />
 
         {/* Header */}

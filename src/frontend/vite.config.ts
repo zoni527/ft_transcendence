5be313c3f3cd014d35c66eba86a25692d80a5fb0
Johnny Varila <jvarila@student.hive.fi>
2026-05-13 13:06:59 +0300

    fix(frontend): making the frontend linter happy by adding an or to make sure clientPort becomes an int

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     host: true,
     port: 5173,
     hmr: {
-      clientPort: parseInt(process.env.VITE_NGINX_PORT_EXTERNAL, 10),
+      clientPort: parseInt(process.env.NGINX_PORT_EXTERNAL || '8443', 10),
     },
     watch: {
       usePolling: true,

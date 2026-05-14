07ac66868f61b6b6e0c61f1d4e62ed1051529d90
Lucio <lucio.surcotaype@outlook.com>
2026-05-14 16:10:15 +0300

    feat: create function to generate APIkey

--- src/backend/authorization/apikey.go
+++ src/backend/authorization/apikey.go
@@ -1,5 +1,24 @@
 package authorization
 
-func generateAPIKey(){
-	
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+)
+
+func GenerateAPIKey(userID string) (string, string, error) {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", "", err
+	}
+	randomSecret := hex.EncodeToString(b)
+
+	rawApiKey := fmt.Sprintf("%s.%s", userID, randomSecret)
+
+	return rawApiKey, hashedSecret, nil
+}
+
+func ValidateAPIKey(userID, key string) error {
+
 }

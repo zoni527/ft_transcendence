99a9ffe475ca29b325f580c1343522207f8fdb6c
Lucio <lucio.surcotaype@outlook.com>
2026-05-15 16:27:12 +0300

    refactor: call repository function from handler

--- src/backend/handlers/public.go
+++ src/backend/handlers/public.go
@@ -1,24 +1,31 @@
 package handlers
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 
 	"ft_transcendence/backend/authorization"
+	"ft_transcendence/backend/repository"
 )
 
 func GetAPIKey(c *gin.Context) {
-	id := c.GetString("userID")
-	if !authorization.IsValidUUID() {
+	userID := c.GetString("userID")
+	if !authorization.IsValidUUID(userID) {
 		c.IndentedJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
 		return
 	}
-	apiKey, hashedSecret, err := authorization.generateAPIKey(id)
+	apiKey, randomSecret, err := authorization.GenerateAPIKey(userID)
 	if err != nil {
-		log.Printf("getAPIKey error: %v", err)
+		log.Printf("GetAPIKey error: %v", err)
 		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
 		return
 	}
-
+	if err := repository.SaveAPIKey(userID, randomSecret); err != nil {
+		log.Printf("GetApiKey error: %v", err)
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+		return
+	}
 	c.IndentedJSON(http.StatusCreated, apiKey)
 }
